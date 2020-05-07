65e131677a504e5026cab7d2cd2a8cd8993ad674
TejasTiwari <f20180280@pilani.bits-pilani.ac.in>
2020-05-07 15:28:45 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,14 +8,15 @@ weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const location= search.value
 
-    const url='http://localhost:3000/weather?address='+location
+    const url= '/weather?address='+location
     
     msgOne.textContent= 'Loading'
     msgTwo.textContent= ''
     fetchForecast(url)
 })
 
-const fetchForecast= (url)=>{fetch(url).then((response)=>{
+const fetchForecast= (url)=>{
+    fetch(url).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             msgOne.textContent= data.error
